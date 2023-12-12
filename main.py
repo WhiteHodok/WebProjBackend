c51c7395369b5039595485fd2407a6b2fe799562
WhiteHodok <balanaraus@gmail.com>
2023-12-13 03:36:50 +0600

    A1-StaticRedirect

--- main.py
+++ main.py
@@ -1,21 +1,42 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
-from jinja2 import Environment, FileSystemLoader
+from fastapi.responses import FileResponse, Response
+import os
+
 
 app = FastAPI()
 
-templates = Environment(loader=FileSystemLoader('templates'))
-
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+# Делаем редирект на статику
+@app.get("/", include_in_schema=False)
+async def redirect_to_index():
+    return Response(status_code=302, headers={"Location": "/static/index.html"})
 
-@app.get("/")
-async def read_index():
-    template = templates.get_template('index.html')
-    return HTMLResponse(content=template.render(), status_code=200)
+# Немного обработчиков
+@app.get("/style.css")
+async def get_style():
+    if not os.path.exists("static/css/style.css"):
+        raise HTTPException(status_code=404)
 
-# Другие импорты, модели и ендпоинты
+    return FileResponse(path="static/css/style.css")
 
-# Пример ссылки на статику из шаблона
-# <link rel="stylesheet" href="/static/styles.css">
+
+@app.get("/404.css")
+async def get_404_style():
+    if not os.path.exists("static/css/404.css"):
+        raise HTTPException(status_code=404)
+
+    return FileResponse(path="static/css/404.css")
+
+
+@app.exception_handler(HTTPException)
+async def handle_http_exception(error: HTTPException):
+    if error.status_code == 404:
+        return FileResponse(path="static/404.html")
+    elif error.status_code == 500:
+        return FileResponse(path="static/404.html")
+    else:
+        return error
+
+
